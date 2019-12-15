c4cdbbedbdaf9f544fe95417a030449cd97383ce
Christian <chrell827@gmail.com>
2019-12-15 14:33:29 -0500

    structure

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name) VALUES("Classic Beef Burger");
+INSERT INTO burgers(burger_name) VALUES("Veggie Burger");
+INSERT INTO burgers(burger_name) VALUES("Angus Beef Burger");
+INSERT INTO burgers(burger_name) VALUES("Chicken Burger");
